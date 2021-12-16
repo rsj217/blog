f2533f3ec060509231a3903d677ba1e3f4c01be1
rsj217 <chengcong.mjm@autonavi.com>
2021-12-16 20:46:58 +0800

    theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -68,6 +68,7 @@ source_suffix = {
 # import sphinx_rtd_theme
 
 # html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
 # html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
